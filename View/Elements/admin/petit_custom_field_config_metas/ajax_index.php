4f360a872aa43a92219d348017290acd96559b6b
arata <contact@materializing.net>
2014-12-31 04:44:37 +0900

    fixes Ajax処理用ビューを追加

---
+++ View/Elements/admin/petit_custom_field_config_metas/ajax_index.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * [ADMIN] PetitCustomField
+ *
+ * @link			http://www.materializing.net/
+ * @author			arata
+ * @package			PetitCustomField
+ * @license			MIT
+ */
+$this->BcBaser->element('petit_custom_field_config_metas/index_list');
