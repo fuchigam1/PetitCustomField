ecffa25a9c8626f7ef5a134346d00c273d8d3aea
arata <contact@materializing.net>
2015-01-01 20:01:41 +0900

    add フィールドタイプ切替え時の入力項目切替え機能を実装

--- View/PetitCustomFieldConfigFields/admin/form.php
+++ View/PetitCustomFieldConfigFields/admin/form.php
@@ -8,6 +8,8 @@
  * @license			MIT
  */
 $this->BcBaser->css('PetitCustomField.admin/petit_custom_field', array('inline' => false));
+$this->BcBaser->js(array('PetitCustomField.admin/petit_custom_field'));
+$currentModelName = $this->request->params['models']['PetitCustomFieldConfigField']['className'];
 ?>
 
 <h3>
@@ -30,67 +32,86 @@ $this->BcBaser->css('PetitCustomField.admin/petit_custom_field', array('inline' 
 
 <div id="PetitCustomFieldConfigFieldTable">
 <table cellpadding="0" cellspacing="0" class="form-table section">
-	<tr>
+	<tr id="Row<?php echo $currentModelName . Inflector::camelize('name'); ?>">
 		<th class="col-head">
 			<?php echo $this->BcForm->label('PetitCustomFieldConfigField.name', 'カスタムフィールド名') ?>&nbsp;<span class="required">*</span>
 		</th>
-		<td class="col-input">
+		<td class="col-input" colspan="3">
 			<?php echo $this->BcForm->input('PetitCustomFieldConfigField.name',
 					array('type' => 'text', 'size' => 60, 'maxlength' => 255, 'counter' => true, 'placeholder' => 'カスタムフィールドの名称')) ?>
 			<?php echo $this->BcForm->error('PetitCustomFieldConfigField.name') ?>
 		</td>
 	</tr>
-	<tr>
+	<tr id="Row<?php echo $currentModelName . Inflector::camelize('label_name'); ?>">
 		<th class="col-head">
 			<?php echo $this->BcForm->label('PetitCustomFieldConfigField.label_name', 'ラベル名') ?>&nbsp;<span class="required">*</span>
 		</th>
-		<td class="col-input">
+		<td class="col-input" colspan="3">
 			<?php echo $this->BcForm->input('PetitCustomFieldConfigField.label_name',
 					array('type' => 'text', 'size' => 60, 'maxlength' => 255, 'counter' => true, 'placeholder' => 'ラベルの名称')) ?>
 			<?php echo $this->BcForm->error('PetitCustomFieldConfigField.label_name') ?>
 		</td>
 	</tr>
-	<tr>
+	<tr id="Row<?php echo $currentModelName . Inflector::camelize('field_name'); ?>">
 		<th class="col-head">
 			<?php echo $this->BcForm->label('PetitCustomFieldConfigField.field_name', 'フィールド名') ?>&nbsp;<span class="required">*</span>
 		</th>
-		<td class="col-input">
+		<td class="col-input" colspan="3">
 			<?php echo $this->BcForm->input('PetitCustomFieldConfigField.field_name',
 					array('type' => 'text', 'size' => 60, 'maxlength' => 255, 'counter' => true, 'placeholder' => 'field_name_sample')) ?>
 			<?php echo $this->BcForm->error('PetitCustomFieldConfigField.field_name') ?>
 			<br /><small>※半角英数で入力してください。</small>
 		</td>
 	</tr>
-	<tr>
+	<tr id="Row<?php echo $currentModelName . Inflector::camelize('field_type'); ?>">
 		<th class="col-head">
 			<?php echo $this->BcForm->label('PetitCustomFieldConfigField.field_type', 'フィールドタイプ') ?>&nbsp;<span class="required">*</span>
 		</th>
-		<td class="col-input">
+		<td class="col-input" colspan="3">
 			<?php echo $this->BcForm->input('PetitCustomFieldConfigField.field_type', array('type' => 'select', 'options' => $customFieldConfig['field_type'])) ?>
 			<?php echo $this->BcForm->error('PetitCustomFieldConfigField.field_type') ?>
 		</td>
 	</tr>
-	<tr>
+	<tr id="Row<?php echo $currentModelName . Inflector::camelize('status'); ?>">
 		<th class="col-head">
 			<?php echo $this->BcForm->label('PetitCustomFieldConfigField.status', '利用状態') ?>
 		</th>
 		<td class="col-input">
 			<?php echo $this->BcForm->input('PetitCustomFieldConfigField.status', array('type' => 'checkbox', 'label' => '利用中')) ?>
 			<?php echo $this->BcForm->error('PetitCustomFieldConfigField.status') ?>
-			&nbsp;&nbsp;&nbsp;&nbsp;<small>※利用中の際は入力欄が表示されます。</small>
 		</td>
-	</tr>
-	<tr>
 		<th class="col-head">
 			<?php echo $this->BcForm->label('PetitCustomFieldConfigField.required', '必須設定') ?>
 		</th>
-		<td class="col-input">
+		<td class="col-input" id="Row<?php echo $currentModelName . Inflector::camelize('required'); ?>" colspan="3">
 			<?php echo $this->BcForm->input('PetitCustomFieldConfigField.required', array('type' => 'checkbox', 'label' => '必須入力とする')) ?>
 			<?php echo $this->BcForm->error('PetitCustomFieldConfigField.required') ?>
-			&nbsp;&nbsp;&nbsp;&nbsp;<small>※入力必須マークが表示されます。</small>
 		</td>
 	</tr>
-	<tr>
+	<tr id="Row<?php echo $currentModelName . Inflector::camelize('description'); ?>">
+		<th class="col-head">
+			<?php echo $this->BcForm->label('PetitCustomFieldConfigField.description', '説明文') ?>
+		</th>
+		<td class="col-input" colspan="3">
+			<?php echo $this->BcForm->input('PetitCustomFieldConfigField.description', array('type' => 'textarea', 'rows' => '2')) ?>
+			<?php echo $this->BcForm->error('PetitCustomFieldConfigField.description') ?>
+			<br /><small>※フィールドの説明文</small>
+		</td>
+	</tr>
+	<tr id="Row<?php echo $currentModelName . Inflector::camelize('default_value'); ?>">
+		<th class="col-head">
+			<?php echo $this->BcForm->label('PetitCustomFieldConfigField.default_value', '初期値') ?>
+		</th>
+		<td class="col-input" colspan="3">
+			<?php echo $this->BcForm->input('PetitCustomFieldConfigField.default_value', array('type' => 'text', 'size' => 60, 'maxlength' => 255, 'counter' => true)) ?>
+			<?php echo $this->BcForm->error('PetitCustomFieldConfigField.default_value') ?>
+			<br /><small>※入力欄の初期値を指定できます。</small>
+		</td>
+	</tr>
+</table>
+
+<table cellpadding="0" cellspacing="0" class="form-table section">
+	<tr id="Row<?php echo $currentModelName . Inflector::camelize('validate'); ?>Group">
 		<th class="col-head">
 			<?php echo $this->BcForm->label('PetitCustomFieldConfigField.validate', '入力値チェック') ?>
 		</th>
@@ -99,98 +120,64 @@ $this->BcBaser->css('PetitCustomField.admin/petit_custom_field', array('inline' 
 			<?php echo $this->BcForm->input('PetitCustomFieldConfigField.validate', array('type' => 'select', 'multiple' => 'checkbox', 'options' => $customFieldConfig['validate'])) ?>
 		</td>
 	</tr>
-	<tr>
-		<th class="col-head">
-			<?php echo $this->BcForm->label('PetitCustomFieldConfigField.description', '説明文') ?>
-		</th>
-		<td class="col-input">
-			<?php echo $this->BcForm->input('PetitCustomFieldConfigField.description', array('type' => 'textarea', 'rows' => '2')) ?>
-			<?php echo $this->BcForm->error('PetitCustomFieldConfigField.description') ?>
-			<br /><small>※フィールドの説明文</small>
-		</td>
-	</tr>
-	<tr>
-		<th class="col-head">
-			<?php echo $this->BcForm->label('PetitCustomFieldConfigField.default_value', '初期値') ?>
-		</th>
-		<td class="col-input">
-			<?php echo $this->BcForm->input('PetitCustomFieldConfigField.default_value', array('type' => 'text', 'size' => 60, 'maxlength' => 255, 'counter' => true)) ?>
-			<?php echo $this->BcForm->error('PetitCustomFieldConfigField.default_value') ?>
-			<br /><small>※カスタムフィールド表示の際の、入力欄の初期値を指定できます。</small>
-		</td>
-	</tr>
-	<tr>
+	<tr  id="Row<?php echo $currentModelName . Inflector::camelize('size'); ?>Group">
 		<th class="col-head">
 			テキスト
 		</th>
 		<td class="col-input">
 			<div class="pcf-input-box">
-				<span class="span4">
+				<span class="span4" id="Row<?php echo $currentModelName . Inflector::camelize('size'); ?>">
 			<?php echo $this->BcForm->label('PetitCustomFieldConfigField.size', '入力サイズ') ?>
 			<?php echo $this->BcForm->input('PetitCustomFieldConfigField.size', array('type' => 'text', 'size' => 4, 'placeholder' => '60')) ?>
 			<?php echo $this->BcForm->error('PetitCustomFieldConfigField.size') ?>
 				</span>
-				<span class="span4">
+				<span class="span4" id="Row<?php echo $currentModelName . Inflector::camelize('max_lenght'); ?>">
 			<?php echo $this->BcForm->label('PetitCustomFieldConfigField.max_length', '最大入力文字数') ?>
-			<?php echo $this->BcForm->error('PetitCustomFieldConfigField.max_length') ?>
 			<?php echo $this->BcForm->input('PetitCustomFieldConfigField.max_length', array('type' => 'text', 'size' => 4, 'placeholder' => '255')) ?>
+					<?php echo $this->BcBaser->img('admin/icn_help.png', array('id' => 'helpPetitCustomFieldConfigFieldMaxLength', 'class' => 'btn help', 'alt' => 'ヘルプ')) ?>
+					<div id="helptextPetitCustomFieldConfigFieldMaxLength" class="helptext">
+						<ul>
+							<li>入力すると、指定文字数制限による入力チェックが行われます。</li>
+						</ul>
+					</div>
+			<?php echo $this->BcForm->error('PetitCustomFieldConfigField.max_length') ?>
 				</span>
-				<span class="span4">
+				<span class="span4" id="Row<?php echo $currentModelName . Inflector::camelize('counter'); ?>">
 			<?php echo $this->BcForm->error('PetitCustomFieldConfigField.counter') ?>
 			<?php echo $this->BcForm->input('PetitCustomFieldConfigField.counter', array('type' => 'checkbox', 'label' => '文字数カウンターを表示する')) ?>
 				</span>
 			</div>
 		</td>
 	</tr>
-	<tr>
+	<tr id="Row<?php echo $currentModelName . Inflector::camelize('placeholder'); ?>">
 		<th class="col-head">
 			<?php echo $this->BcForm->label('PetitCustomFieldConfigField.placeholder', 'プレースホルダー') ?>
 		</th>
 		<td class="col-input">
 			<?php echo $this->BcForm->input('PetitCustomFieldConfigField.placeholder', array('type' => 'text', 'size' => 60, 'placeholder' => 'プレースホルダー表示例')) ?>
 			<?php echo $this->BcForm->error('PetitCustomFieldConfigField.placeholder') ?>
-			<br /><small>※入力欄内に表示される文字を指定できます。</small>
+			<br /><small>※入力欄内に未入力時に表示される文字を指定できます。</small>
 		</td>
 	</tr>
-	<tr>
-		<th class="col-head">
-			<?php echo $this->BcForm->label('PetitCustomFieldConfigField.prepend', '入力欄前に表示') ?>
-		</th>
-		<td class="col-input">
-			<?php echo $this->BcForm->input('PetitCustomFieldConfigField.prepend', array('type' => 'text', 'size' => 60)) ?>
-			<?php echo $this->BcForm->error('PetitCustomFieldConfigField.prepend') ?>
-			<br /><small>※入力欄の前に表示される文字を指定できます。</small>
-		</td>
-	</tr>
-	<tr>
-		<th class="col-head">
-			<?php echo $this->BcForm->label('PetitCustomFieldConfigField.append', '入力欄後に表示') ?>
-		</th>
-		<td class="col-input">
-			<?php echo $this->BcForm->input('PetitCustomFieldConfigField.append', array('type' => 'text', 'size' => 60)) ?>
-			<?php echo $this->BcForm->error('PetitCustomFieldConfigField.append') ?>
-			<br /><small>※入力欄の後に表示される文字を指定できます。</small>
-		</td>
-	</tr>
-	<tr>
+	<tr id="Row<?php echo $currentModelName . Inflector::camelize('rows'); ?>Group">
 		<th class="col-head">
 			テキストエリア
 		</th>
 		<td class="col-input">
 			<div class="pcf-input-box">
-				<span class="span4">
+				<span class="span4" id="Row<?php echo $currentModelName . Inflector::camelize('rows'); ?>">
 			<?php echo $this->BcForm->label('PetitCustomFieldConfigField.rows', '行数') ?>
-			<?php echo $this->BcForm->input('PetitCustomFieldConfigField.rows', array('type' => 'text', 'size' => 4, 'placeholder' => '3')) ?>
+			<?php echo $this->BcForm->input('PetitCustomFieldConfigField.rows', array('type' => 'text', 'size' => 5, 'placeholder' => '3')) ?>
 			<?php echo $this->BcForm->error('PetitCustomFieldConfigField.rows') ?>
 					<br /><small>※Wysiwyg Editorの場合は〜px指定となります。</small>
 				</span>
-				<span class="span4">
+				<span class="span4"id="Row<?php echo $currentModelName . Inflector::camelize('cols'); ?>">
 			<?php echo $this->BcForm->label('PetitCustomFieldConfigField.cols', '横幅サイズ') ?>
-			<?php echo $this->BcForm->input('PetitCustomFieldConfigField.cols', array('type' => 'text', 'size' => 4, 'placeholder' => '40')) ?>
+			<?php echo $this->BcForm->input('PetitCustomFieldConfigField.cols', array('type' => 'text', 'size' => 5, 'placeholder' => '40')) ?>
 			<?php echo $this->BcForm->error('PetitCustomFieldConfigField.cols') ?>
 					<br /><small>※Wysiwyg Editorの場合は〜％指定となります。</small>
 				</span>
-				<span class="span4">
+				<span class="span4" id="Row<?php echo $currentModelName . Inflector::camelize('editor_tool_type'); ?>">
 			<?php echo $this->BcForm->label('PetitCustomFieldConfigField.editor_tool_type', 'Ckeditorのタイプ') ?>
 			<?php echo $this->BcForm->error('PetitCustomFieldConfigField.editor_tool_type') ?>
 			<?php echo $this->BcForm->input('PetitCustomFieldConfigField.editor_tool_type', array('type' => 'select', 'options' => $customFieldConfig['editor_tool_type'])) ?>
@@ -198,7 +185,7 @@ $this->BcBaser->css('PetitCustomField.admin/petit_custom_field', array('inline' 
 			</div>
 		</td>
 	</tr>
-	<tr>
+	<tr id="Row<?php echo $currentModelName . Inflector::camelize('choices'); ?>">
 		<th class="col-head">
 			<?php echo $this->BcForm->label('PetitCustomFieldConfigField.choices', '選択肢') ?>
 		</th>
@@ -211,7 +198,7 @@ $this->BcBaser->css('PetitCustomField.admin/petit_custom_field', array('inline' 
 			<br /><small>指定したいラベル名と値を半角「:」で区切って入力してください。（例：ラベル名:値）</small>
 		</td>
 	</tr>
-	<tr>
+	<tr id="Row<?php echo $currentModelName . Inflector::camelize('separator'); ?>">
 		<th class="col-head">
 			<?php echo $this->BcForm->label('PetitCustomFieldConfigField.separator', '区切り文字') ?>
 		</th>
@@ -221,7 +208,7 @@ $this->BcBaser->css('PetitCustomField.admin/petit_custom_field', array('inline' 
 			<br /><small>※ラジオボタン表示の際の区切り文字を指定できます。</small>
 		</td>
 	</tr>
-	<tr>
+	<tr id="Row<?php echo $currentModelName . Inflector::camelize('auto_convert'); ?>">
 		<th class="col-head">
 			入力テキスト変換処理
 		</th>
@@ -229,6 +216,26 @@ $this->BcBaser->css('PetitCustomField.admin/petit_custom_field', array('inline' 
 			<?php echo $this->BcForm->label('PetitCustomFieldConfigField.auto_convert', '自動変換') ?>
 			<?php echo $this->BcForm->error('PetitCustomFieldConfigField.auto_convert') ?>
 			<?php echo $this->BcForm->input('PetitCustomFieldConfigField.auto_convert', array('type' => 'select', 'options' => $customFieldConfig['auto_convert'])) ?>			
+		</td>
+	</tr>
+	<tr id="Row<?php echo $currentModelName . Inflector::camelize('prepend'); ?>">
+		<th class="col-head">
+			<?php echo $this->BcForm->label('PetitCustomFieldConfigField.prepend', '入力欄前に表示') ?>
+		</th>
+		<td class="col-input">
+			<?php echo $this->BcForm->input('PetitCustomFieldConfigField.prepend', array('type' => 'text', 'size' => 60)) ?>
+			<?php echo $this->BcForm->error('PetitCustomFieldConfigField.prepend') ?>
+			<br /><small>※入力欄の前に表示される文字を指定できます。</small>
+		</td>
+	</tr>
+	<tr id="Row<?php echo $currentModelName . Inflector::camelize('append'); ?>">
+		<th class="col-head">
+			<?php echo $this->BcForm->label('PetitCustomFieldConfigField.append', '入力欄後に表示') ?>
+		</th>
+		<td class="col-input">
+			<?php echo $this->BcForm->input('PetitCustomFieldConfigField.append', array('type' => 'text', 'size' => 60)) ?>
+			<?php echo $this->BcForm->error('PetitCustomFieldConfigField.append') ?>
+			<br /><small>※入力欄の後に表示される文字を指定できます。</small>
 		</td>
 	</tr>
 </table>
---
+++ webroot/js/admin/petit_custom_field.js
@@ -0,0 +1,270 @@
+/**
+ * [ADMIN] PetitCustomField
+ *
+ * @link			http://www.materializing.net/
+ * @author			arata
+ * @package			PetitCustomField
+ * @license			MIT
+ */
+/**
+ * プチカスタムフィールド用のJS処理
+ */
+$(function(){
+	// タイプを選択すると入力するフィールドが切り替わる
+	$("#PetitCustomFieldConfigFieldFieldType").change(function(){
+		changeFieldSetting($("#PetitCustomFieldConfigFieldFieldType").val());
+	});
+	changeFieldSetting($("#PetitCustomFieldConfigFieldFieldType").val());
+	
+	// カスタムフィールド名の入力時、ラベル名が空の場合は名称を自動で入力する
+	$("#PetitCustomFieldConfigFieldName").change(function(){
+		if(!$("#PetitCustomFieldConfigFieldLabelName").val()){
+			$("#PetitCustomFieldConfigFieldLabelName").val($("#PetitCustomFieldConfigFieldName").val());
+		}
+	});
+
+/**
+ * タイプの値によってフィールドの表示設定を行う
+ */
+	function changeFieldSetting(value){
+		console.log(value);
+		switch ($("#PetitCustomFieldConfigFieldFieldType").val()){
+			case 'text':
+				// バリデーション項目
+				$("#RowPetitCustomFieldConfigFieldValidateGroup").show('slow');
+					$("#PetitCustomFieldConfigFieldValidateHANKAKUCHECK").parent().show('slow');
+					$("#PetitCustomFieldConfigFieldValidateNUMERICCHECK").parent().show('slow');
+					$("#PetitCustomFieldConfigFieldValidateNONCHECKCHECK").parent().hide('fast');
+				
+				$("#RowPetitCustomFieldConfigFieldSizeGroup").show('slow');
+					$("#RowPetitCustomFieldConfigFieldSize").show('slow');
+					$("#RowPetitCustomFieldConfigFieldMaxLenght").show('slow');
+					$("#RowPetitCustomFieldConfigFieldCounter").show('slow');
+				$("#RowPetitCustomFieldConfigFieldPlaceholder").show('slow');
+				
+				$("#RowPetitCustomFieldConfigFieldRowsGroup").hide('fast');
+					$("#RowPetitCustomFieldConfigFieldRows").hide('fast');
+					$("#PetitCustomFieldConfigFieldCols").hide('fast');
+					$("#RowPetitCustomFieldConfigFieldEditorToolType").hide('fast');
+					
+				$("#RowPetitCustomFieldConfigFieldChoices").hide('fast');
+				$("#RowPetitCustomFieldConfigFieldSeparator").hide('fast');
+				$("#RowPetitCustomFieldConfigFieldAutoConvert").show('slow');
+				break;
+				
+			case 'textarea':
+				// バリデーション項目
+				$("#RowPetitCustomFieldConfigFieldValidateGroup").show('slow');
+					$("#PetitCustomFieldConfigFieldValidateHANKAKUCHECK").parent().show('slow');
+					$("#PetitCustomFieldConfigFieldValidateNUMERICCHECK").parent().show('slow');
+					$("#PetitCustomFieldConfigFieldValidateNONCHECKCHECK").parent().hide('fast');
+				
+				$("#RowPetitCustomFieldConfigFieldSizeGroup").show('slow');
+					$("#RowPetitCustomFieldConfigFieldSize").hide('fast');
+					$("#RowPetitCustomFieldConfigFieldMaxLenght").hide('fast');
+					$("#RowPetitCustomFieldConfigFieldCounter").show('slow');
+				$("#RowPetitCustomFieldConfigFieldPlaceholder").show('slow');
+				
+				$("#RowPetitCustomFieldConfigFieldRowsGroup").show('slow');
+					$("#RowPetitCustomFieldConfigFieldRows").show('slow');
+					$("#PetitCustomFieldConfigFieldCols").show('slow');
+					$("#RowPetitCustomFieldConfigFieldEditorToolType").hide('fast');
+					
+				$("#RowPetitCustomFieldConfigFieldChoices").hide('fast');
+				$("#RowPetitCustomFieldConfigFieldSeparator").hide('fast');
+				$("#RowPetitCustomFieldConfigFieldAutoConvert").show('slow');
+				break;
+				
+			case 'date':
+				// バリデーション項目
+				$("#RowPetitCustomFieldConfigFieldValidateGroup").hide('fast');
+					$("#PetitCustomFieldConfigFieldValidateHANKAKUCHECK").parent().hide('fast');
+					$("#PetitCustomFieldConfigFieldValidateNUMERICCHECK").parent().hide('fast');
+					$("#PetitCustomFieldConfigFieldValidateNONCHECKCHECK").parent().hide('fast');
+				
+				$("#RowPetitCustomFieldConfigFieldSizeGroup").hide('fast');
+					$("#RowPetitCustomFieldConfigFieldSize").hide('fast');
+					$("#RowPetitCustomFieldConfigFieldMaxLenght").hide('fast');
+					$("#RowPetitCustomFieldConfigFieldCounter").hide('fast');
+				$("#RowPetitCustomFieldConfigFieldPlaceholder").hide('fast');
+				
+				$("#RowPetitCustomFieldConfigFieldRowsGroup").hide('fast');
+					$("#RowPetitCustomFieldConfigFieldRows").hide('fast');
+					$("#PetitCustomFieldConfigFieldCols").hide('fast');
+					$("#RowPetitCustomFieldConfigFieldEditorToolType").hide('fast');
+					
+				$("#RowPetitCustomFieldConfigFieldChoices").hide('fast');
+				$("#RowPetitCustomFieldConfigFieldSeparator").hide('fast');
+				$("#RowPetitCustomFieldConfigFieldAutoConvert").hide('fast');
+				break;
+				
+			case 'datetime':
+				$("#RowPetitCustomFieldConfigFieldValidateGroup").hide('fast');
+					$("#PetitCustomFieldConfigFieldValidateHANKAKUCHECK").parent().hide('fast');
+					$("#PetitCustomFieldConfigFieldValidateNUMERICCHECK").parent().hide('fast');
+					$("#PetitCustomFieldConfigFieldValidateNONCHECKCHECK").parent().hide('fast');
+					
+				$("#RowPetitCustomFieldConfigFieldSizeGroup").hide('fast');
+					$("#RowPetitCustomFieldConfigFieldSize").hide('fast');
+					$("#RowPetitCustomFieldConfigFieldMaxLenght").hide('fast');
+					$("#RowPetitCustomFieldConfigFieldCounter").hide('fast');
+				$("#RowPetitCustomFieldConfigFieldPlaceholder").hide('fast');
+				
+				$("#RowPetitCustomFieldConfigFieldRowsGroup").hide('fast');
+					$("#RowPetitCustomFieldConfigFieldRows").hide('fast');
+					$("#PetitCustomFieldConfigFieldCols").hide('fast');
+					$("#RowPetitCustomFieldConfigFieldEditorToolType").hide('fast');
+					
+				$("#RowPetitCustomFieldConfigFieldChoices").hide('fast');
+				$("#RowPetitCustomFieldConfigFieldSeparator").hide('fast');
+				$("#RowPetitCustomFieldConfigFieldAutoConvert").hide('fast');
+				break;
+				
+			case 'select':
+				$("#RowPetitCustomFieldConfigFieldValidateGroup").hide('fast');
+					$("#PetitCustomFieldConfigFieldValidateHANKAKUCHECK").parent().hide('fast');
+					$("#PetitCustomFieldConfigFieldValidateNUMERICCHECK").parent().hide('fast');
+					$("#PetitCustomFieldConfigFieldValidateNONCHECKCHECK").parent().hide('fast');
+					
+				$("#RowPetitCustomFieldConfigFieldSizeGroup").hide('fast');
+					$("#RowPetitCustomFieldConfigFieldSize").hide('fast');
+					$("#RowPetitCustomFieldConfigFieldMaxLenght").hide('fast');
+					$("#RowPetitCustomFieldConfigFieldCounter").hide('fast');
+				$("#RowPetitCustomFieldConfigFieldPlaceholder").hide('fast');
+				
+				$("#RowPetitCustomFieldConfigFieldRowsGroup").hide('fast');
+					$("#RowPetitCustomFieldConfigFieldRows").hide('fast');
+					$("#PetitCustomFieldConfigFieldCols").hide('fast');
+					$("#RowPetitCustomFieldConfigFieldEditorToolType").hide('fast');
+					
+				$("#RowPetitCustomFieldConfigFieldChoices").show('slow');
+				$("#RowPetitCustomFieldConfigFieldSeparator").hide('fast');
+				$("#RowPetitCustomFieldConfigFieldAutoConvert").hide('fast');
+				break;
+				
+			case 'radio':
+				$("#RowPetitCustomFieldConfigFieldValidateGroup").hide('fast');
+					$("#PetitCustomFieldConfigFieldValidateHANKAKUCHECK").parent().hide('fast');
+					$("#PetitCustomFieldConfigFieldValidateNUMERICCHECK").parent().hide('fast');
+					$("#PetitCustomFieldConfigFieldValidateNONCHECKCHECK").parent().hide('fast');
+					
+				$("#RowPetitCustomFieldConfigFieldSizeGroup").hide('fast');
+					$("#RowPetitCustomFieldConfigFieldSize").hide('fast');
+					$("#RowPetitCustomFieldConfigFieldMaxLenght").hide('fast');
+					$("#RowPetitCustomFieldConfigFieldCounter").hide('fast');
+				$("#RowPetitCustomFieldConfigFieldPlaceholder").hide('fast');
+				
+				$("#RowPetitCustomFieldConfigFieldRowsGroup").hide('fast');
+					$("#RowPetitCustomFieldConfigFieldRows").hide('fast');
+					$("#PetitCustomFieldConfigFieldCols").hide('fast');
+					$("#RowPetitCustomFieldConfigFieldEditorToolType").hide('fast');
+					
+				$("#RowPetitCustomFieldConfigFieldChoices").show('slow');
+				$("#RowPetitCustomFieldConfigFieldSeparator").show('slow');
+				$("#RowPetitCustomFieldConfigFieldAutoConvert").hide('fast');
+				break;
+				
+			case 'checkbox':
+				// バリデーション項目
+				$("#RowPetitCustomFieldConfigFieldValidateGroup").hide('fast');
+					$("#PetitCustomFieldConfigFieldValidateHANKAKUCHECK").parent().hide('fast');
+					$("#PetitCustomFieldConfigFieldValidateNUMERICCHECK").parent().hide('fast');
+					$("#PetitCustomFieldConfigFieldValidateNONCHECKCHECK").parent().show('fast');
+					
+				$("#RowPetitCustomFieldConfigFieldSizeGroup").hide('fast');
+					$("#RowPetitCustomFieldConfigFieldSize").hide('fast');
+					$("#RowPetitCustomFieldConfigFieldMaxLenght").hide('fast');
+					$("#RowPetitCustomFieldConfigFieldCounter").hide('fast');
+				$("#RowPetitCustomFieldConfigFieldPlaceholder").hide('fast');
+				
+				$("#RowPetitCustomFieldConfigFieldRowsGroup").hide('fast');
+					$("#RowPetitCustomFieldConfigFieldRows").hide('fast');
+					$("#PetitCustomFieldConfigFieldCols").hide('fast');
+					$("#RowPetitCustomFieldConfigFieldEditorToolType").hide('fast');
+					
+				$("#RowPetitCustomFieldConfigFieldChoices").hide('fast');
+				$("#RowPetitCustomFieldConfigFieldSeparator").hide('fast');
+				$("#RowPetitCustomFieldConfigFieldAutoConvert").hide('fast');
+				break;
+				
+			case 'multiple':
+				// バリデーション項目
+				$("#RowPetitCustomFieldConfigFieldValidateGroup").show('slow');
+					$("#PetitCustomFieldConfigFieldValidateHANKAKUCHECK").parent().hide('fast');
+					$("#PetitCustomFieldConfigFieldValidateNUMERICCHECK").parent().hide('fast');
+					$("#PetitCustomFieldConfigFieldValidateNONCHECKCHECK").parent().show('slow');
+				
+				$("#RowPetitCustomFieldConfigFieldSizeGroup").hide('fast');
+					$("#RowPetitCustomFieldConfigFieldSize").hide('fast');
+					$("#RowPetitCustomFieldConfigFieldMaxLenght").hide('fast');
+					$("#RowPetitCustomFieldConfigFieldCounter").hide('fast');
+				$("#RowPetitCustomFieldConfigFieldPlaceholder").hide('fast');
+				
+				$("#RowPetitCustomFieldConfigFieldRowsGroup").hide('fast');
+					$("#RowPetitCustomFieldConfigFieldRows").hide('fast');
+					$("#PetitCustomFieldConfigFieldCols").hide('fast');
+					$("#RowPetitCustomFieldConfigFieldEditorToolType").hide('fast');
+					
+				$("#RowPetitCustomFieldConfigFieldChoices").show('slow');
+				$("#RowPetitCustomFieldConfigFieldSeparator").show('slow');
+				$("#RowPetitCustomFieldConfigFieldAutoConvert").hide('fast');
+				break;
+			case 'pref':
+				// バリデーション項目
+				$("#RowPetitCustomFieldConfigFieldValidateGroup").hide('fast');
+					$("#PetitCustomFieldConfigFieldValidateHANKAKUCHECK").parent().hide('fast');
+					$("#PetitCustomFieldConfigFieldValidateNUMERICCHECK").parent().hide('fast');
+					$("#PetitCustomFieldConfigFieldValidateNONCHECKCHECK").parent().hide('fast');
+					
+				$("#RowPetitCustomFieldConfigFieldSizeGroup").hide('fast');
+					$("#RowPetitCustomFieldConfigFieldSize").hide('fast');
+					$("#RowPetitCustomFieldConfigFieldMaxLenght").hide('fast');
+					$("#RowPetitCustomFieldConfigFieldCounter").hide('fast');
+				$("#RowPetitCustomFieldConfigFieldPlaceholder").hide('fast');
+				
+				$("#RowPetitCustomFieldConfigFieldRowsGroup").hide('fast');
+					$("#RowPetitCustomFieldConfigFieldRows").hide('fast');
+					$("#PetitCustomFieldConfigFieldCols").hide('fast');
+					$("#RowPetitCustomFieldConfigFieldEditorToolType").hide('fast');
+					
+				$("#RowPetitCustomFieldConfigFieldChoices").hide('fast');
+				$("#RowPetitCustomFieldConfigFieldSeparator").hide('fast');
+				$("#RowPetitCustomFieldConfigFieldAutoConvert").hide('fast');
+				break;
+				
+			case 'wysiwyg':
+				// バリデーション項目
+				$("#RowPetitCustomFieldConfigFieldValidateGroup").hide('fast');
+					$("#PetitCustomFieldConfigFieldValidateHANKAKUCHECK").parent().hide('fast');
+					$("#PetitCustomFieldConfigFieldValidateNUMERICCHECK").parent().hide('fast');
+					$("#PetitCustomFieldConfigFieldValidateNONCHECKCHECK").parent().hide('fast');
+					
+				$("#RowPetitCustomFieldConfigFieldSizeGroup").hide('fast');
+					$("#RowPetitCustomFieldConfigFieldSize").hide('fast');
+					$("#RowPetitCustomFieldConfigFieldMaxLenght").hide('fast');
+					$("#RowPetitCustomFieldConfigFieldCounter").hide('fast');
+				$("#RowPetitCustomFieldConfigFieldPlaceholder").hide('fast');
+				
+				$("#RowPetitCustomFieldConfigFieldRowsGroup").show('slow');
+					$("#RowPetitCustomFieldConfigFieldRows").show('slow');
+						$("#PetitCustomFieldConfigFieldRows").attr('placeholder', '200px');
+					$("#PetitCustomFieldConfigFieldCols").show('slow');
+						$("#PetitCustomFieldConfigFieldCols").attr('placeholder', '100%');
+					$("#RowPetitCustomFieldConfigFieldEditorToolType").show('slow');
+					
+				$("#RowPetitCustomFieldConfigFieldChoices").hide('fast');
+				$("#RowPetitCustomFieldConfigFieldSeparator").hide('fast');
+				$("#RowPetitCustomFieldConfigFieldAutoConvert").hide('fast');
+				break;
+			
+			case 'file':
+				// バリデーション項目
+				$("#RowPetitCustomFieldConfigFieldValidateGroup").hide('fast');
+					$("#PetitCustomFieldConfigFieldValidateHANKAKUCHECK").parent().hide('fast');
+					$("#PetitCustomFieldConfigFieldValidateNUMERICCHECK").parent().hide('fast');
+					$("#PetitCustomFieldConfigFieldValidateNONCHECKCHECK").parent().hide('fast');
+				break;
+				
+		}
+	}
+});
