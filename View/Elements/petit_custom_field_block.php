e070fb566ad1492764d6d628748469ffb6eafa6c
arata <contact@materializing.net>
2015-01-04 07:21:20 +0900

    fixes 表示調整

--- View/Elements/petit_custom_field_block.php
+++ View/Elements/petit_custom_field_block.php
@@ -9,6 +9,8 @@
  */
 $this->BcBaser->css('PetitCustomField.petit_custom_field');
 ?>
+<?php if ($this->PetitCustomField->allowPublish($this->PetitCustomField->publicConfigData, 'PetitCustomFieldConfig')): ?>
+
 <?php if (!empty($post)): ?>
 	<?php if (!empty($post['PetitCustomField'])): ?>
 <div id="PetitCustomFieldBlock">
@@ -16,16 +18,15 @@ $this->BcBaser->css('PetitCustomField.petit_custom_field');
 		<table class="table">
 			<thead>
 				<tr>
-					<th>名称（フィールド名）</th><td>内容</td>
+					<th>フィールド名</th><th>ラベル名</th><td>内容</td>
 				</tr>
 			</thead>
 			<tbody>
 				<?php foreach ($post['PetitCustomField'] as $fieldName => $value): ?>
 				<tr>
-					<th>ラベル名（<?php echo $fieldName ?>）</th><td><?php echo $this->PetitCustomField->getPdcfDataField($fieldName) ?></td>
-				</tr>
-				<tr>
-					<th>入力値（<?php echo $fieldName ?>）</th><td><?php echo $this->PetitCustomField->getPdcfData($post, $fieldName) ?></td>
+					<td><?php echo $fieldName ?></td>
+					<td><?php echo $this->PetitCustomField->getPdcfDataField($fieldName) ?></td>
+					<td><?php echo $this->PetitCustomField->getPdcfData($post, $fieldName) ?></td>
 				</tr>
 				<?php endforeach ?>
 			</tbody>
@@ -33,4 +34,6 @@ $this->BcBaser->css('PetitCustomField.petit_custom_field');
 	</div>
 </div>
 	<?php endif ?>
+<?php endif ?>
+
 <?php endif ?>
