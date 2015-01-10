0accd0f4a85a24a26f4be1f9dd4de69e0693084d
arata <contact@materializing.net>
2015-01-11 05:41:56 +0900

    fixes フィールド一覧のヘルプに説明追加

--- View/Elements/admin/helps/petit_custom_field_metas_index.php
+++ View/Elements/admin/helps/petit_custom_field_metas_index.php
@@ -10,6 +10,10 @@
 ?>
 <p>フィールド設定一覧管理では、コンテンツに設定中のカスタムフィールドを確認できます。</p>
 <ul>
-	<li>操作欄の <?php $this->BcBaser->img('admin/icn_tool_manage.png') ?> ボタンからは、カスタムフィールドを別コンテンツへ移すことができる編集画面へ移動できます。</li>
-	<li>操作欄の <?php $this->BcBaser->img('admin/icn_tool_edit.png') ?> ボタンからは、カスタムフィールド設定の編集画面へ移動できます。</li>
+	<li>操作欄の <?php $this->BcBaser->img('admin/icn_tool_manage.png') ?> ボタンからは、フィールドを別コンテンツへ移すことができる編集画面へ移動できます。</li>
+	<li>操作欄の <?php $this->BcBaser->img('admin/icn_tool_edit.png') ?> ボタンからは、フィールド設定の編集画面へ移動できます。</li>
+	<li>操作欄の <?php $this->BcBaser->img('admin/icn_tool_up.png') ?> ボタンでは、フィールドをひとつ上の並び順へ移動できます。</li>
+	<li>操作欄の <?php $this->BcBaser->img('admin/icn_tool_down.png') ?> ボタンでは、フィールドをひとつ下の並び順へ移動できます。</li>
+	<li>操作欄の <i class="fa fa-arrow-circle-up fa-2x" style="vertical-align: bottom;margin-left: 2px;"></i> ボタンでは、フィールドを最上段へ移動できます。</li>
+	<li>操作欄の <i class="fa fa-arrow-circle-down fa-2x" style="vertical-align: bottom;margin-left: 2px;"></i> ボタンでは、フィールドを最下段へ移動できます。</li>
 </ul>
