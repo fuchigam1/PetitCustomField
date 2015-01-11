feca0c285ef0585af9e7e3ea0ea940e4035d2dde
arata <contact@materializing.net>
2015-01-12 00:03:33 +0900

    fixes ブログ設定に表示するリンク表示を調整

--- Event/PetitCustomFieldHelperEventListener.php
+++ Event/PetitCustomFieldHelperEventListener.php
@@ -46,11 +46,13 @@ class PetitCustomFieldHelperEventListener extends BcHelperEventListener {
 			if ($View->request->action == 'admin_edit') {
 				// ブログ設定編集画面にプチ・カスタムフィールドメタ設定一覧リンクを表示する
 				if ($event->data['id'] == 'BlogContentAdminEditForm') {
-					$output = $View->BcBaser->link('≫プチ・カスタムフィールド設定', array(
+					$output = '<div id="PetitCustomFieldConfigBox">';
+					$output .= $View->BcBaser->getLink('≫プチ・カスタムフィールド設定', array(
 						'plugin' => 'petit_custom_field',
 						'controller' => 'petit_custom_field_config_metas',
 						'action' => 'index', $View->viewVars['blogContent']['PetitCustomFieldConfig']['id']
 					));
+					$output .= '</div>';
 					$event->data['out'] = $event->data['out'] . $output;
 				}
 			}
