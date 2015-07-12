9a0702041c5b579f8aaa7ef24957b7ec2a6f23ea
arata <contact@materializing.net>
2015-07-12 21:05:44 +0900

    fixed インストール時のデータ生成を廃止
    - カスタムフィールド設定データありきの仕様を見直し

--- Config/init.php
+++ Config/init.php
@@ -4,33 +4,3 @@
  * データベース初期化
  */
 $this->Plugin->initDb('plugin', 'PetitCustomField');
-
-/**
- * ブログ情報を元にデータを作成する
- *   ・設定データがないブログ用のデータのみ作成する
- * 
- */
-	App::uses('BlogContent', 'Blog.Model');
-	$BlogContentModel = new BlogContent();
-	$blogContentDatas = $BlogContentModel->find('list', array('recursive' => -1));
-	if ($blogContentDatas) {
-		CakePlugin::load('PetitCustomField');
-		App::uses('PetitCustomFieldAppModel', 'PetitCustomField.Model');
-		App::uses('PetitCustomFieldConfig', 'PetitCustomField.Model');
-		$PetitCustomFieldConfigModel = new PetitCustomFieldConfig();
-		foreach ($blogContentDatas as $key => $blog) {
-			$petitCustomFieldConfig = $PetitCustomFieldConfigModel->findByContentId($key);
-			$savaData = array();
-			if(!$petitCustomFieldConfig) {
-				$savaData['PetitCustomFieldConfig']['content_id'] = $key;
-				$savaData['PetitCustomFieldConfig']['status'] = false;
-				$savaData['PetitCustomFieldConfig']['model'] = 'BlogContent';
-				$savaData['PetitCustomFieldConfig']['form_place'] = 'normal';
-				$PetitCustomFieldConfigModel->create($savaData);
-				$PetitCustomFieldConfigModel->save($savaData, array(
-					'validate' => false,
-					'callbacks' => false
-				));
-			}
-		}
-	}
