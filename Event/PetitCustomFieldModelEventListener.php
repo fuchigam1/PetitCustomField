a280d5a8f0b0877479819437fb1247801681c9ec
arata <contact@materializing.net>
2015-07-12 21:53:33 +0900

    fixed カスタムフィールド設定データ作成機能追加に伴い、ブログコンテンツ保存時のカスタムフィールド設定データ保存機能を廃止
    - カスタムフィールド設定データありきの仕様を見直し

--- Event/PetitCustomFieldModelEventListener.php
+++ Event/PetitCustomFieldModelEventListener.php
@@ -20,7 +20,6 @@ class PetitCustomFieldModelEventListener extends BcModelEventListener {
 		'Blog.BlogPost.afterDelete',
 		'Blog.BlogPost.beforeValidate',
 		'Blog.BlogContent.beforeFind',
-		'Blog.BlogContent.afterSave',
 		'Blog.BlogContent.afterDelete',
 	);
 	
@@ -439,25 +438,6 @@ class PetitCustomFieldModelEventListener extends BcModelEventListener {
 	}
 	
 /**
- * blogBlogContentAfterSave
- * 
- * @param CakeEvent $event
- */
-	public function blogBlogContentAfterSave(CakeEvent $event) {
-		$Model = $event->subject();
-		$created = $event->data[0];
-		if ($created) {
-			$contentId = $Model->getLastInsertId();
-			$saveData = $this->generateContentSaveData($Model, $contentId);
-			// ブログ設定追加時に設定情報を保存する
-			$this->PetitCustomFieldConfigModel->create($saveData);
-			if (!$this->PetitCustomFieldConfigModel->save()) {
-				$this->log(sprintf('ID：%s のプチ・カスタムフィールド設定の保存に失敗しました。', $Model->data['PetitCustomFieldConfig']['id']));
-			}
-		}
-	}
-	
-/**
  * blogBlogContentAfterDelete
  * 
  * @param CakeEvent $event
@@ -545,76 +525,6 @@ class PetitCustomFieldModelEventListener extends BcModelEventListener {
 					} else {
 						// コピー元データがない時
 						$data['PetitCustomField']['blog_post_id'] = $modelId;
-					}
-				}
-				break;
-				
-			default:
-				break;
-		}
-		
-		return $data;
-	}
-	
-/**
- * 保存するデータの生成
- * 
- * @param Object $Model
- * @param int $contentId
- * @return array
- */
-	public function generateContentSaveData($Model, $contentId) {
-		$params = Router::getParams();
-		$this->setup();
-		
-		$data = array();
-		if ($Model->alias == 'BlogContent') {
-			$modelId = $contentId;
-			if (isset($params['pass'][0])) {
-				$oldModelId = $params['pass'][0];
-			}
-		}
-		
-		if ($contentId) {
-			$data = $this->PetitCustomFieldConfigModel->find('first', array('conditions' => array(
-				'PetitCustomFieldConfig.content_id' => $contentId
-			)));
-		}
-		
-		switch ($params['action']) {
-			case 'admin_add':
-				// 追加時
-				if (!empty($Model->data['PetitCustomFieldConfig'])) {
-					$data['PetitCustomFieldConfig'] = $Model->data['PetitCustomFieldConfig'];
-				}
-				$data['PetitCustomFieldConfig']['content_id'] = $contentId;
-				$data['PetitCustomFieldConfig']['model'] = $Model->alias;
-				break;
-				
-			case 'admin_edit':
-				// 編集時
-				$data['PetitCustomFieldConfig'] = array_merge($data['PetitCustomFieldConfig'], $Model->data['PetitCustomFieldConfig']);
-				break;
-				
-			case 'admin_ajax_copy':
-				// Ajaxコピー処理時に実行
-				// ブログコピー保存時にエラーがなければ保存処理を実行
-				if (empty($Model->validationErrors)) {
-					$_data = $this->PetitCustomFieldConfigModel->find('first', array(
-						'conditions' => array(
-							'PetitCustomFieldConfig.content_id' => $oldModelId
-						),
-						'recursive' => -1
-					));
-					// XXX もし設定の初期データ作成を行ってない事を考慮して判定している
-					if ($_data) {
-						// コピー元データがある時
-						$data = Hash::merge($data, $_data);
-						$data['PetitCustomFieldConfig']['content_id'] = $contentId;
-						unset($data['PetitCustomFieldConfig']['id']);
-					} else {
-						// コピー元データがない時
-						$data['PetitCustomFieldConfig']['content_id'] = $modelId;
 					}
 				}
 				break;
