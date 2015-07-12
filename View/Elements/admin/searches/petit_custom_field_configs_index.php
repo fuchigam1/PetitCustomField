f3e65a3945641e5fbb8e867682881601620cfc04
arata <contact@materializing.net>
2015-07-12 21:06:32 +0900

    add カスタムフィールド設定一覧画面で、利用状態での検索条件を追加

--- View/Elements/admin/searches/petit_custom_field_configs_index.php
+++ View/Elements/admin/searches/petit_custom_field_configs_index.php
@@ -14,6 +14,10 @@
 		<?php echo $this->BcForm->label('PetitCustomFieldConfig.content_id', 'ブログ') ?>
 		&nbsp;<?php echo $this->BcForm->input('PetitCustomFieldConfig.content_id', array('type' => 'select', 'options' => $blogContentDatas)) ?>
 	</span>
+	<span>
+		<?php echo $this->BcForm->label('PetitCustomFieldConfig.status', '利用状態') ?>
+		&nbsp;<?php echo $this->BcForm->input('PetitCustomFieldConfig.status', array('type' => 'select', 'options' => $this->BcText->booleanMarkList(), 'empty' => '指定なし')) ?>
+	</span>
 </p>
 <div class="button">
 	<?php echo $this->BcForm->submit('admin/btn_search.png', array('alt' => '検索', 'class' => 'btn'), array('div' => false, 'id' => 'BtnSearchSubmit')) ?>
